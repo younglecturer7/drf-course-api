6d8d747ad63e0a2eba479922f344e7eb2fd33521
Young Lecturer <younglecturer7@gmail.com>
2025-08-15 18:08:49 +0100

    model updated

--- api/models.py
+++ api/models.py
@@ -14,7 +14,7 @@ class Product(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.PositiveIntegerField()
-    image = models.ImageField(upload_to='products/', null=True, blank=True)
+    image = models.ImageField(upload_to="products/", null=True, blank=True)
 
     # Custom method to check if product is in stock
     @property
@@ -23,31 +23,33 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
-    
+
 
 class Order(models.Model):
     class statusChoices(models.TextChoices):
-        PENDING = 'P', 'Pending'
-        COMPLETED = 'C', 'Completed'
-        CANCELLED = 'X', 'Cancelled'
+        PENDING = "P", "Pending"
+        COMPLETED = "C", "Completed"
+        CANCELLED = "X", "Cancelled"
 
-    order_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
+    order_id = models.UUIDField(
+        primary_key=True, default=uuid.uuid4, editable=False, unique=True
+    )
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    product = models.ManyToManyField(Product, through='OrderItem', related_name='orders')
+    product = models.ManyToManyField(
+        Product, through="OrderItem", related_name="orders"
+    )
     status = models.CharField(
-        max_length=1,
-        choices=statusChoices.choices,
-        default=statusChoices.PENDING
+        max_length=1, choices=statusChoices.choices, default=statusChoices.PENDING
     )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"Order {self.order_id} by {self.user.username}"
-    
+
 
 class OrderItem(models.Model):
-    order = models.ForeignKey(Order, related_name='items', on_delete=models.CASCADE)
+    order = models.ForeignKey(Order, related_name="items", on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=1)
 
@@ -57,6 +59,6 @@ class OrderItem(models.Model):
 
     def __str__(self):
         return f"{self.quantity} of {self.product.name} in Order {self.order.id}"
-    
+
     class Meta:
-        unique_together = ('order', 'product')
+        unique_together = ("order", "product")
